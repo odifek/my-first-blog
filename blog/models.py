7081caf27a348056243a44c3b65a6305ddccb839
kennedy <kennedy998@gmail.com>
2015-09-09 20:00:14 +0100

    Extended the application to display individual posts when clicked

--- blog/models.py
+++ blog/models.py
@@ -12,8 +12,6 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    
+
     def __str__(self):
         return self.title
-      
-
